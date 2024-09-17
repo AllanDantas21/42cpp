fc894ca95f9242fb2b22935a41e517673afbcb73
Allandantas21 <Allandantas21.ad@gmail.com>
2024-09-17 12:05:01 -0300

    brain to deep copy

--- cpp04/ex01/Cat.cpp
+++ cpp04/ex01/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 22:01:45 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/03 00:41:00 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/17 12:01:27 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,25 @@ Cat::Cat(){
 
 Cat::Cat(const Cat& src): Animal(){
     std::cout << "Cat copy constructor called\n";
-    *this = src;
+    this->_brain = new Brain(*src._brain);
+    this->type = src.type;
 }
 
 /* Destructor */
 Cat::~Cat(){
     std::cout << "Cat " << this->type << " destroyed\n";
-     delete this->_brain;
+    delete this->_brain;
 }
 
 /* Overload */
 Cat& Cat::operator=(Cat const& other){
     if (this != &other) {
         this->type = other.getType();
+        delete this->_brain;
+        this->_brain = new Brain(*other._brain);
     }
     return *this;
 }
-
 
 /* Methods */
 void Cat::makeSound() const{
--- cpp04/ex01/Dog.cpp
+++ cpp04/ex01/Dog.cpp
@@ -6,41 +6,46 @@
 /*   By: aldantas <aldantas@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 22:01:45 by aldantas          #+#    #+#             */
-/*   Updated: 2024/09/03 00:41:05 by aldantas         ###   ########.fr       */
+/*   Updated: 2024/09/17 12:01:53 by aldantas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
-/* Construtors */
-Dog::Dog(){
+/* Constructors */
+Dog::Dog() {
     std::cout << "Dog default constructor called\n";
     this->type = "Dog";
+    this->_brain = new Brain();
 }
 
-Dog::Dog(const Dog& src): Animal() {
+Dog::Dog(const Dog& src) : Animal() {
     std::cout << "Dog copy constructor called\n";
+    this->_brain = new Brain(*src._brain);
     *this = src;
 }
 
 /* Destructor */
-Dog::~Dog(){
+Dog::~Dog() {
+    delete this->_brain;
     std::cout << "Dog " << this->type << " destroyed\n";
 }
 
 /* Overload */
-Dog& Dog::operator=(Dog const& other){
+Dog& Dog::operator=(Dog const& other) {
     if (this != &other) {
         this->type = other.getType();
+        delete this->_brain;
+        this->_brain = new Brain(*other._brain);
     }
     return *this;
 }
 
 /* Methods */
-void Dog::makeSound() const{
+void Dog::makeSound() const {
     std::cout << "woof woof\n";
 }
 
-Brain* Dog::getBrain() const{
+Brain* Dog::getBrain() const {
     return this->_brain;
 }
