4b9677e020ea697c4a8495def644a5d6a537adda
allandantas21 <allandantas21.ad@gmail.com>
2024-11-07 22:57:39 -0300

    atts

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -18,12 +18,11 @@ int main()
 	std::string *stringPTR = &str;
 	std::string& stringREF = str;
 
-
 	std::cout << &str << '\n';
 	std::cout << stringPTR << '\n';
 	std::cout << &stringREF << '\n';
 
 	std::cout << str << '\n';
 	std::cout << *stringPTR << '\n';
-   	std::cout << stringREF << '\n';
+ 	std::cout << stringREF << '\n';
 }
