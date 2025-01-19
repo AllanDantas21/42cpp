bc0c77876c5e2dd0b30f03104c0321cc6e6a3e42
allandantas21 <allandantas21.ad@gmail.com>
2025-01-19 13:22:57 -0300

    fix cpp6/ex02 main

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -13,7 +13,7 @@ Base* generate(void) {
         case 0: return new A();
         case 1: return new B();
         case 2: return new C();
-        default: return nullptr;
+        default: return NULL;
     }
 }
 
