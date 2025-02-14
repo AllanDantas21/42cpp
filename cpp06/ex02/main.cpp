d5bd30e5146796985dc0f4b7fd447dfcaf01109e
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-02-14 01:17:32 -0300

    refacts

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -10,9 +10,9 @@
 Base* generate(void) {
     std::srand(std::clock());
     switch (std::rand() % 3) {
-        case 0: return new A();
-        case 1: return new B();
-        case 2: return new C();
+        case 0: return (new A());
+        case 1: return (new B());
+        case 2: return (new C());
         default: return NULL;
     }
 }
