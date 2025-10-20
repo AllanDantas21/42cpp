7981482374770cbf0353bf42e4f8968bfa268d26
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-10-19 21:09:07 -0300

    remove .h identations

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -23,45 +23,22 @@ private:
     
     std::vector<std::pair<int, int> > createPairsDeque(std::deque<int>& container, int& unpaired);
     std::vector<std::pair<int, int> > createPairsList(std::list<int>& container, int& unpaired);
-    
     void insertElementsDeque(std::deque<int>& result, const std::vector<int>& elements);
     void insertElementsList(std::list<int>& result, const std::vector<int>& elements);
-    
-    void insertPendElementsDeque(std::deque<int>& result, 
-        const std::vector<int>& pendChain, 
-        const std::vector<int>& mainChain);
-    void insertPendElementsList(std::list<int>& result, 
-        const std::vector<int>& pendChain, 
-        const std::vector<int>& mainChain);
-    
+    void insertPendElementsDeque(std::deque<int>& result, const std::vector<int>& pendChain, const std::vector<int>&);
+    void insertPendElementsList(std::list<int>& result, const std::vector<int>& pendChain, const std::vector<int>&);
     void insertUnpaired(std::deque<int>& result, int unpaired);
     void insertUnpaired(std::list<int>& result, int unpaired);
-    
     void sortPairsByComparison(std::vector<std::pair<int, int> >& pairs);
     std::vector<int> generateJacobsthalSequence(int n);
-    
-    void separatePairsIntoChainsDeque(const std::vector<std::pair<int, int> >& pairs,
-                                      std::vector<int>& mainChain,
-                                      std::vector<int>& pendChain);
-    void separatePairsIntoChainsList(const std::vector<std::pair<int, int> >& pairs,
-                                     std::vector<int>& mainChain,
-                                     std::vector<int>& pendChain);
-    
+    void separatePairsIntoChainsDeque(const std::vector<std::pair<int, int> >& pairs, std::vector<int>& mainChain, std::vector<int>& pendChain);
+    void separatePairsIntoChainsList(const std::vector<std::pair<int, int> >& pairs, std::vector<int>& mainChain, std::vector<int>& pendChain);
     void recursiveSortMainChainDeque(std::vector<int>& mainChain);
     void recursiveSortMainChainList(std::vector<int>& mainChain);
-    
-    void buildSortedResultDeque(std::deque<int>& result,
-                                const std::vector<int>& mainChain,
-                                const std::vector<int>& pendChain,
-                                int unpaired);
-    void buildSortedResultList(std::list<int>& result,
-                               const std::vector<int>& mainChain,
-                               const std::vector<int>& pendChain,
-                               int unpaired);
-    
+    void buildSortedResultDeque(std::deque<int>& result, const std::vector<int>& mainChain, const std::vector<int>& pendChain, int unpaired);
+    void buildSortedResultList(std::list<int>& result, const std::vector<int>& mainChain, const std::vector<int>& pendChain, int unpaired);
     void parseInput(int ac, char** av);
     bool isValidNumber(const std::string& str);
-    
     double measureSortTimeDeque(std::deque<int>& container);
     double measureSortTimeList(std::list<int>& container);
 
